438b78289edb62134b396417677a884624e2cdbe
L1MJH <lolflgc@icloud.com>
2023-10-03 17:51:50 +0800

    Update testscript.sh

--- test/testscript.sh
+++ test/testscript.sh
@@ -1,1 +1,5 @@
-sed -i 's/&v=[0-9]*/&v=$((`date +%s`/86400))/' README.md
+# Calculate the value we want
+days_since_epoch=$((`date +%s`/86400))
+
+# Use sed to replace the value
+sed -i '' -e "s/\(&v=\)[0-9]*/\1$days_since_epoch/g" README_test.md
